1dbe7ea844c4266e40e163ab9c583a45ac339b9b
adriianzambrano <adrianx_13@hotmail.com>
2023-01-15 09:57:30 -0500

    homework Realizado

--- 01-Node/homework/01Exercises/bash.js
+++ 01-Node/homework/01Exercises/bash.js
@@ -1,7 +1,25 @@
-const process = require("process");
+
+const process = require("process"); // process nativo de Node - solo hay que traerlo
 const commands = require("./commands/index.js");
 
+function print(output){
+  process.stdout.write(output)
+  process.stdout.write( "\nprompt > ")
+}
 function bash() {
+  process.stdout.write("prompt > ")
+  process.stdin.on('data', (data)=>{
+    var args = data.toString().trim().split(" ")
+    var cmd = args.shift()
+    //console.log("---> ", args)
+    if(commands.hasOwnProperty(cmd)){
+      commands[cmd](print, args.join(" "))
+    }else{
+      print(`command not found: ${cmd}`)
+    }
+  
+
+  })
 }
 
 bash();
--- 01-Node/homework/01Exercises/commands/index.js
+++ 01-Node/homework/01Exercises/commands/index.js
@@ -2,20 +2,54 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd())
+}
 
-function date() {}
+function date(print) {
+    print(Date())
+}
 
-function echo() {}
+function echo(print, args) {
+    print(args)
+}
 
-function ls() {}
+function ls(print) {
+    fs.readdir(".", function(error, files){
+        if(error)throw new Error(error)
+        print(files.join(" "))
 
-function cat() {}
+    })
+}
 
-function head() {}
+function cat(print, args) {
+    fs.readFile(args, 'utf-8', function(error, data){
+        if(error)throw new Error(error)
+        print(data);
+    })
+}
 
-function tail() {}
+function head(print, args) {
+    fs.readFile(args, 'utf-8', (error, data)=>{
+        if(error) throw new Error(error)
+        var lines = data.split('\n')
+        print(lines[0].trim())
+    })
+}
 
-function curl() {}
+function tail(print, args) {
+    fs.readFile(args, 'utf-8', (error, data)=>{
+        if(error) throw new Error(error)
+        var ultimalinea = data.split('\n')
+        print(ultimalinea[ultimalinea.length-1].trim())
+    })
+}
 
-module.exports = {};
+function curl(print, args) {
+    utils.request(args, (error, response)=>{
+        if(error) throw new Error(error)
+        print(response)
+    })
+}
+
+module.exports = {pwd, date, echo, ls, cat, head, tail, curl};
